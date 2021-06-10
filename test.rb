18496789c648b1e70fd33b1f33d6be7fa2c27d36
Jonathan Serafini <jonathan.serafini@gmail.com>
2021-06-10 15:01:57 +0200

    Add files via upload

---
+++ test.rb
@@ -0,0 +1,26 @@
+
+
+# Le site doit comprendre :
+
+
+  # Un titre, un sous titre et un paragraphe
+  # Une image
+  # Une liste
+  # Une division avec le bord du cadre de couleur et arrondi et avec une ombre
+  # Une modale issue de Bootstrap
+  # Une navbar faite par vous identifiée par la classe .my-navbar
+  # L’utilisation des Flexboxs
+  # Une police issue de Google fonts
+  # Une vidéo issue de YouTube
+  # Une carte issue de Google maps
+  # Un formulaire issue de Type form ou MailChimp
+
+
+
+
+
+
+
+
+
+
